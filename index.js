27d2805d90f04a33bfe9ffbdff9e1a2fbc65021c
Celia Ait Ouazzou <celiaaitouazzou@gmail.com>
2024-08-24 17:41:42 -0400

    New File Added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const body = document.getElementsByTagName("body")[0];
+const mainToRemove = body.querySelector("main#main");
+
+body.removeChild(mainToRemove);
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = " CELIA is the champion";
